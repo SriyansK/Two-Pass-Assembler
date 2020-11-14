ab82098f641ce76a683faa5adc851ea8bc6d4ad3
Sriyans <srkr.2805@gmail.com>
2020-11-14 14:23:14 +0530

    updated literal check

--- assembler.cpp
+++ assembler.cpp
@@ -1,33 +1,16 @@
 #include <bits/stdc++.h>
-#define ll long long int
 #define mp make_pair
 #define pb push_back
-#define pp pop_back
 #define fr first
 #define sc second
 #define ins insert
-#define lb lower_bound
-#define ub upper_bound
 #define ii pair<int,int>
 #define iii pair<int,pair<int,int>>
 #define vi vector <int>
-#define ascii_a 97
-#define ascii_z 48
-#define ascii_A 65
-#define pi 3.14159265358979323846
 using namespace std;
- 
-const ll N =1e5+10;
-const int MOD=1e9+7;
-const long long IINF=1e18;
 
 int pos;
 int current_line=0,literal_counter=10000;
-
-
-/*
-    correct literals according to mnemonic and perfect the location counter.
-*/
 
 bool islabel(string label)
 {
@@ -41,6 +24,25 @@ bool ismnemonic(string operand,unordered_map <string,int> mnemonic)
     if(mnemonic.find(operand)==mnemonic.end())
         return false;
     return true;
+}
+
+bool one_operand(string temp)
+{
+    unordered_map <string,int> ump;
+    ump["data"]=-2,ump["ldc"]=0,ump["adc"]=1,ump["ldl"]=2,ump["stl"]=3,ump["ldnl"]=4,ump["stnl"]=5,ump["10"]=6,ump["call"]=13;
+    ump["brz"]=15,ump["brlz"]=16,ump["br"]=17,ump["SET"]=-4;
+    if(ump.find(temp)!=ump.end())
+        return true;
+    return false;
+}
+
+bool zero_operand(string temp)
+{
+    unordered_map <string,int> ump;
+    ump["add"]=6,ump["sub"]=7,ump["shl"]=8,ump["shr"]=9,ump["a2sp"]=11,ump["sp2a"]=12,ump["return"]=14,ump["HALT"]=18;
+    if(ump.find(temp)!=ump.end())
+        return true;
+    return false;
 }
 
 string sep(string s)
@@ -75,35 +77,12 @@ bool isdigits(string temp)
     return true;
 }
 
-void printlabel(unordered_map <string,int> symtab)
-{
-    for(auto it:symtab)
-        cout<<it.first<<" "<<it.second<<endl;
-}
-
-void printlit(vector <pair<string,int>> litab)
-{
-    for(auto it:litab)
-        cout<<it.first<<" "<<it.second<<endl;
-}
-
-void printerror(vector <string> errors)
-{
-    for(auto s:errors)
-        cout<<s<<endl;
-}
-
 int main()
 {
-    ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-
-    //taking input from file
     fstream input_file;
-    input_file.open("test4.txt",ios::in);
+    input_file.open("test3.txt",ios::in);
 
-    //removing comments and bogus lines
-    vector <string> code; // stores the separated input
+    vector <string> code; // remove bogus lines and comments and store it in code
     vector <string> errors; //stores the errors
     if(input_file.is_open())
     {
@@ -140,14 +119,13 @@ int main()
 
     unordered_map<string,int>  symtab; //symbol table
     vector<pair<string,int>>  litab; // literal table 1st is value and 2nd is address
-    for(string s:code)
+    for(string s:code) // Here we make symbol table and literal table
     {
         pos=0;
         string temp=sep(s);
         if(islabel(temp)) //If label the 1st string is a label
         {
-            //process label
-            if(symtab.find(temp)==symtab.end())
+            if(symtab.find(temp)==symtab.end()) // Here we add labels to symbol table
                 symtab[temp]=current_line;
             else if(symtab.find(temp)!=symtab.end() && symtab[temp]!=-1)
             {
@@ -155,89 +133,120 @@ int main()
                 errors.pb(error);
             }
             else if(symtab.find(temp)!=symtab.end() && symtab[temp]==-1)
-                symtab[temp]=current_line;
-            // process label ends
-            temp="";temp=sep(s); // find the 2nd string
-            if(temp.length()==0) // if string is empty then continue;
-            {
-                current_line++;
-                continue;
-            }
-            if(ismnemonic(temp,mnemonic)) //check if mnemonic
-            {
-                temp="";temp=sep(s);//3rd string
-                string temp1="";temp1=sep(s);//if 4th string
-                if(temp1.length()>0)
-                {
-                    string error="ERROR:Extra variable at line "; error+=current_line+'0';
-                    errors.pb(error);
-                    current_line++;
-                    continue;
-                }
-                if(temp.length()==0)//if empty
-                {
-                    current_line++;
-                    continue;
-                }
-                if(islabel(temp))// If label is declared after mnemonic 
-                {
-                    string error="ERROR:Wrong Syntax at line"; error+=current_line+'0';
-                    errors.pb(error);
-                }
-                if(!isdigit(temp[0]) && temp[0]!='-' && temp[0]!='+' && symtab.find(temp+":")==symtab.end())//if label is used after mnemonic then insert in map
-                    symtab[temp+":"]=-1;
-                else if(!isdigit(temp[0]) && temp[0]!='-' && temp[0]!='+' && symtab.find(temp+":")!=symtab.end())
-                    continue;
-                else if((temp[0]=='-' || temp[0]=='+') && isdigits(temp.substr(1,temp.length())))//literal table
-                    litab.pb({temp,literal_counter++});
-                else if(isdigits(temp))
-                    litab.pb({temp,literal_counter++});
-                else
-                {
-                    string error="ERROR:Wrong Syntax at line "; error+=current_line+'0';
-                    errors.pb(error);
-                }
-            }
-            else
-            {
-                string error="ERROR:Wrong Syntax at line "; error+=current_line+'0';
-                errors.pb(error);
-            }
-        }
-        else if(ismnemonic(temp,mnemonic)) // If Mnemonic
-        {
-            // find after mnemonic
-            temp="";temp=sep(s);
-            string temp1="";temp1=sep(s);//if 4th string
-            if(temp1.length()>0)
-            {
-                string error="ERROR:Extra variable at line "; error+=current_line+'0';
-                errors.pb(error);
-                current_line++;
-                continue;
-            }
+                symtab[temp]=current_line; // Here adding conditions end
+            
+            temp="";temp=sep(s); // here we check 2nd string
             if(temp.length()==0)
             {
                 current_line++;
                 continue;
             }
-            if(islabel(temp))// If label is declared after mnemonic 
+            else if(ismnemonic(temp,mnemonic)) 
             {
-                string error="ERROR:Wrong Syntax at line "; error+=current_line+'0';
-                errors.pb(error);
-            }
-            if(!isdigit(temp[0]) && temp[0]!='-' && temp[0]!='+' && symtab.find(temp+":")==symtab.end())//if label is used after mnemonic then insert in map
-                symtab[temp+":"]=-1;
-            else if(!isdigit(temp[0]) && temp[0]!='-' && temp[0]!='+' && symtab.find(temp+":")!=symtab.end())
+                string mnemo=temp;//store mnemonic in string
+                temp="";temp=sep(s); // Here we check if there are more than 1 operand
+                string temp1="";temp1=sep(s);
+                if(temp1.length()>0) 
+                {
+                    string error="ERROR:Extra operand at line "; error+=current_line+'0';
+                    errors.pb(error);
+                    current_line++;
                     continue;
-            else if((temp[0]=='-' || temp[0]=='+') && isdigits(temp.substr(1,temp.length())))//literal table
-                litab.pb({temp,literal_counter++});
-            else if(isdigits(temp))
-                litab.pb({temp,literal_counter++});
+                } // Here checking ends
+
+                if(zero_operand(mnemo))
+                {
+                    if(temp.length()>0)
+                    {
+                        string error="ERROR:Extra Operand at line "; error+=current_line+'0';
+                        errors.pb(error);
+                        current_line++;
+                        continue;
+                    }
+                }
+                else if(one_operand(mnemo))
+                {
+                    if(temp.length()==0)
+                    {
+                        string error="ERROR:Missing operand at line "; error+=current_line+'0';
+                        errors.pb(error);
+                    }
+                    else if(islabel(temp))
+                    {
+                        string error="ERROR:Label declared after mnemonic"; error+=current_line+'0';
+                        errors.pb(error);
+                    }
+                    else if(((temp[0]<='z' && temp[0]>='a')||(temp[0]<='Z' && temp[0]>='A')) && symtab.find(temp+":")==symtab.end())//if label is used after mnemonic then insert in map
+                        symtab[temp+":"]=-1; //new label found
+                    else if(((temp[0]<='z' && temp[0]>='a')||(temp[0]<='Z' && temp[0]>='A') )&& symtab.find(temp+":")!=symtab.end())
+                    {
+                        current_line++; //label already in table
+                        continue;
+                    }
+                    else if(((temp[0]=='-' || temp[0]=='+') && isdigits(temp.substr(1,temp.length())))||isdigits(temp))//literal table
+                        litab.pb({temp,literal_counter++}); // is a number
+                    else
+                    {
+                        string error="ERROR:Wrong Syntax at line "; error+=current_line+'0';
+                        errors.pb(error);
+                    }
+                }
+            }
             else
             {
                 string error="ERROR:Wrong Syntax at line "; error+=current_line+'0';
                 errors.pb(error);
+            } // Here checking ends
+        }
+        else if(ismnemonic(temp,mnemonic)) // If Mnemonic
+        {
+            string mnemo=temp;//store mnemonic in string
+            temp="";temp=sep(s); // Here we check if there are more than 1 operand
+            string temp1="";temp1=sep(s);
+            if(temp1.length()>0) 
+            {
+                string error="ERROR:Extra operand at line "; error+=current_line+'0';
+                errors.pb(error);
+                current_line++;
+                continue;
+            } // Here checking ends
+
+            if(zero_operand(mnemo))
+            {
+                if(temp.length()>0)
+                {
+                    string error="ERROR:Extra Operand at line "; error+=current_line+'0';
+                    errors.pb(error);
+                    current_line++;
+                    continue;
+                }
+            }
+            else if(one_operand(mnemo))
+            {
+                if(temp.length()==0)
+                {
+                    string error="ERROR:Missing operand at line "; error+=current_line+'0';
+                    errors.pb(error);
+                }
+                else if(islabel(temp))
+                {
+                    string error="ERROR:Label declared after mnemonic"; error+=current_line+'0';
+                    errors.pb(error);
+                }
+                else if(((temp[0]<='z' && temp[0]>='a')||(temp[0]<='Z' && temp[0]>='A')) && symtab.find(temp+":")==symtab.end())//if label is used after mnemonic then insert in map
+                    symtab[temp+":"]=-1; // new label found
+                else if(((temp[0]<='z' && temp[0]>='a')||(temp[0]<='Z' && temp[0]>='A') )&& symtab.find(temp+":")!=symtab.end())
+                {
+                    current_line++; //label already in table
+                    continue;
+                }
+                else if(((temp[0]=='-' || temp[0]=='+') && isdigits(temp.substr(1,temp.length())))||isdigits(temp))//literal table
+                    litab.pb({temp,literal_counter++}); // is a number
+                else
+                {
+                    string error="ERROR:Wrong Syntax at line "; error+=current_line+'0';
+                    errors.pb(error);
+                }
             }
         }
         else
@@ -250,11 +259,6 @@ int main()
     for(auto it:symtab)
         if(it.second==-1)
         {
-            string error="ERROR:Undeclared label";
-            errors.pb(error);
+            string error="ERROR:Undeclared label";errors.pb(error);
         }
-    /*printlabel(symtab);
-    printlit(litab);
-    printerror(errors);*/
-    
 }
